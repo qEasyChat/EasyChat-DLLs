3031a82a07a68712b076aca6089856c3857f5235
q1e123 <robertnicolaesolca@gmail.com>
2021-11-12 14:59:33 +0200

    added Utils and modified to be DLL compatible

---
+++ EasyChat-DLLs/Utils.cpp
@@ -0,0 +1,67 @@
+#include "pch.h"
+#include "Utils.h"
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+void Utils::memory_error()
+{
+	std::cerr << "MEMORY ERROR: server connection" << std::endl;
+	exit(EXIT_FAILURE);
+}
+
+size_t Utils::string_to_size_t(std::string string)
+{
+
+	std::stringstream sstream(string);
+	size_t result;
+	sstream >> result;
+	return result;
+}
+
+std::string Utils::get_query(std::string file)
+{
+	std::string query = "";
+	std::fstream query_file(file);
+	std::string line;
+	while (getline(query_file, line)) {
+		query += line + " ";
+	}
+
+	return query;
+}
+
+std::string Utils::replace(std::string string, std::string substring_to_replace, std::string replacement)
+{
+
+	size_t start_position = string.find(substring_to_replace);
+	string.replace(start_position, substring_to_replace.length(), replacement);
+	return string;
+}
+
+size_t Utils::gcd(size_t a, size_t b)
+{
+	size_t mod_result;
+	while (true) {
+		mod_result = a % b;
+		if (mod_result == 0)
+		{
+			return b;
+		}
+		a = b;
+		b = mod_result;
+	}
+}
+
+std::string Utils::double_vector_to_string(std::vector<double> v)
+{
+	auto result = Utils::vector_to_string(v);
+	return result;
+}
+
+std::vector<double> Utils::string_to_double_vector(std::string string)
+{
+	auto result = Utils::string_to_vector<double>(string);
+	return result;
+}
---
+++ EasyChat-DLLs/Utils.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <sstream>
+#include <string>
+#include <vector>
+
+#ifdef UTILS_EXPORTS
+#define UTILS_API __declspec(dllexport)
+#else
+#define UTILS_API __declspec(dllimport)
+#endif
+
+
+namespace Utils
+{
+	void UTILS_API memory_error();
+
+	size_t UTILS_API string_to_size_t(std::string string);
+	size_t UTILS_API gcd(size_t a, size_t b);
+
+	std::string UTILS_API get_query(std::string file);
+
+	std::string UTILS_API replace(std::string string, std::string substring_to_replace, std::string replacement);
+
+
+	std::string UTILS_API double_vector_to_string(std::vector<double> v);
+	std::vector<double> UTILS_API string_to_double_vector(std::string string);
+
+	template <class T>
+	std::string vector_to_string(std::vector<T> v)
+	{
+		std::stringstream string_stream;
+		std::copy(v.begin(), v.end(), std::ostream_iterator<T>(string_stream, " "));
+		std::string result = string_stream.str();
+		return result;
+	}
+
+	template <class T>
+	std::vector<T> string_to_vector(std::string str)
+	{
+		std::vector<T> result;
+		std::stringstream stream(str);
+		T element;
+		while (stream >> element)
+		{
+			result.push_back(element);
+		}
+		return result;
+	}
+}
