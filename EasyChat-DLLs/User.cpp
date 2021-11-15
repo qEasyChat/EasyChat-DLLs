842d5f4fa57791b2c752a996197f5660296c1026
q1e123 <robertnicolaesolca@gmail.com>
2021-11-15 08:14:29 +0200

    added User

---
+++ EasyChat-DLLs/User.cpp
@@ -0,0 +1,41 @@
+#include "pch.h"
+
+#include "User.h"
+
+User::~User()
+{
+	this->id = 0;
+	this->username = "";
+	this->password_hash = "";
+}
+
+
+size_t User::get_id() const
+{
+	return id;
+}
+
+void User::set_id(const size_t id)
+{
+	this->id = id;
+}
+
+std::string User::get_username() const
+{
+	return username;
+}
+
+void User::set_username(const std::string& username)
+{
+	this->username = username;
+}
+
+std::string User::get_password_hash() const
+{
+	return password_hash;
+}
+
+void User::set_password_hash(const std::string& password_hash)
+{
+	this->password_hash = password_hash;
+}
---
+++ EasyChat-DLLs/User.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <string>
+
+#ifdef USER_EXPORTS
+#define USER_API __declspec(dllexport)
+#else
+#define USER_API __declspec(dllimport)
+#endif
+
+class USER_API User
+{
+public:
+	User(){}
+	User(const size_t id, const std::string& username, const std::string& password_hash)
+		: id(id),
+		  username(username),
+		  password_hash(password_hash)
+	{
+	}
+
+	~User();
+	size_t get_id() const;
+	void set_id(const size_t id);
+	std::string get_username() const;
+	void set_username(const std::string& username);
+	std::string get_password_hash() const;
+	void set_password_hash(const std::string& password_hash);
+private:
+	size_t id;
+private:
+	std::string username, password_hash;
+};
+
