f7eb05f6a35bcf5a546ddd38a27a83423a175430
q1e123 <robertnicolaesolca@gmail.com>
2021-11-18 08:16:41 +0200

    added Client

---
+++ EasyChat-DLLs/Client.cpp
@@ -0,0 +1,109 @@
+#include "pch.h"
+
+#include "Client.h"
+
+#include "Crypto_Manager.h"
+#include "Utils.h"
+
+Client::Client(int port_number, const std::string ip)
+{
+	this->server_connection = std::shared_ptr<Connection>(new Connection(port_number, ip));
+	if(this->server_connection == nullptr)
+	{
+		Utils::memory_error();
+	}
+	std::memset(server_addr.sin_zero, '\0', sizeof(server_addr.sin_zero));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port_number);
+	server_addr.sin_addr.s_addr = inet_addr(ip.c_str());
+}
+
+Client::~Client() {
+	if (reciver.joinable()) {
+		reciver.join();
+	}
+	this->server_name = "";
+	this->server_connection.reset();
+}
+
+void Client::connect_and_auth(std::string username, std::string password) {
+	try
+	{
+		connect_to_server();
+	}
+	catch (Server_Down_Exception exception)
+	{
+		std::cerr << "server is down" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	try
+	{
+		authentification(username, password);
+	}
+	catch (Login_Exception exception)
+	{
+		std::cerr << "authentification was not successful" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+void Client::connect_to_server()
+{
+	SOCKET client_socket = this->server_connection->get_socket();
+
+	if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+		throw Server_Down_Exception();
+	}
+	std::cout << "connection was successful" << std::endl;
+
+}
+
+void Client::authentification(std::string username, std::string password)
+{
+	std::string password_hash = crypto_manager.get_sha3_512_hash(password);
+	this->server_connection->send_message(username);
+	this->server_connection->send_message(password_hash);
+	this->server_name = this->server_connection->recive_message();
+	
+	std::string login_response = this->server_connection->recive_message();
+	if (login_response == "RETRY") {
+		throw Login_Exception();
+	}
+	std::cout << "login was successful" << std::endl;
+	this->server_connection->set_username(username);
+}
+
+void Client::start_reciver()
+{
+	this->reciver = std::thread(&Client::recive_message, this);
+}
+
+void Client::recive_message() {
+	std::string message = "";
+	while (message!= "SOCKET_DOWN") {
+		message = this->server_connection->recive_message();
+		if(message != "" || message != "\n")
+		{
+			std::cout << message << std::endl;
+		}
+	}
+}
+
+void Client::sender() {
+	while (true) {
+		std::string message = "";
+		std::getline(std::cin, message);
+		this->server_connection->send_message(message);
+	}
+}
+
+std::string Client::get_server_name()
+{
+	return this->server_name;
+}
+
+void Client::set_server_name(std::string server_name)
+{
+	this->server_name = server_name;
+}
---
+++ EasyChat-DLLs/Client.h
@@ -0,0 +1,59 @@
+#pragma once
+#pragma comment( lib, "ws2_32.lib")
+
+#include <thread>
+#include <iostream>
+#include <string>
+
+#include <winsock2.h>
+
+#include "Connection.h"
+#include "Crypto_Manager.h"
+
+#ifdef CLIENT_EXPORTS
+	#define CLIENT_API __declspec(dllexport)
+#else
+	#define CLIENT_API __declspec(dllexport)
+#endif
+
+class CLIENT_API Server_Down_Exception : public std::exception {
+public:
+	virtual const char* what() const throw() {
+		return "Server is down";
+	}private:
+};
+
+class CLIENT_API Login_Exception : public std::exception {
+public:
+	virtual const char* what() const throw() {
+		return "Authentification failed";
+	}
+private:
+};
+
+class CLIENT_API Client
+{
+public:
+	Client(int port_number, const std::string ip);
+	~Client();
+
+	void connect_and_auth(std::string username, std::string password);
+	void start_reciver();
+	void sender();
+
+	std::string get_server_name();
+	void set_server_name(std::string server_name);
+private:
+	std::string server_name;
+	std::thread reciver;
+
+	std::shared_ptr<Connection> server_connection;
+	struct sockaddr_in server_addr;
+
+	Crypto_Manager crypto_manager;
+
+	void connect_to_server();
+	void authentification(std::string username, std::string password);
+	void recive_message();
+};
+
